aaaf5e3d81d9f4fa1103666fcee6959339702ec4
arielaa86 <arielaa86@gmail.com>
2022-07-19 12:28:07 +0200

    Fix name

--- CounterButton.js
+++ CounterButton.js
@@ -1,4 +1,4 @@
-const ButtonCounter = (props) => {
+const CounterButton = (props) => {
 
     const {start, label} = {...props}
     const [count, setCount] = React.useState(+start)
@@ -6,12 +6,12 @@ const ButtonCounter = (props) => {
     return <button type="button" onClick={()=> setCount(count +1 )}>{label} ({count})</button>
 }
 
-const buttons = document.getElementsByTagName('ButtonCounter');
+const buttons = document.getElementsByTagName('CounterButton');
 
 [...buttons].forEach(element => {
     const props = element.getAttributeNames().reduce((acc, name) => {
         return {...acc, [name]: element.getAttribute(name)};
     }, {});
 
-    ReactDOM.render(<ButtonCounter props={props} />, element );
+    ReactDOM.render(<CounterButton props={props} />, element );
 });
